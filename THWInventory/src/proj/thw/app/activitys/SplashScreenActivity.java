e9ebef882e5902ecf02c722f1002ebc5e2d9a229
max <max@192.168.28.108>
2014-01-08 20:32:29 +0100

    init folder and default.csv

--- THWInventory/src/proj/thw/app/activitys/SplashScreenActivity.java
+++ THWInventory/src/proj/thw/app/activitys/SplashScreenActivity.java
@@ -5,12 +5,16 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FilenameFilter;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Locale;
 
 import proj.thw.app.R;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
 import android.util.Log;
 import android.widget.ArrayAdapter;
@@ -24,13 +28,15 @@ public class SplashScreenActivity extends Activity {
 	
 	//private static final String appFolder = "//storage//"
 	private static final String IMPORT_EXPORT_FILE_FOLDER 	= "IE";
-	private static final String DEFAULT_FILE 				= "default.csv";
+	private static final String DEFAULT_FILE_NAME 			= "default";
+	private static final String FILE_EXTENTION 				= ".csv";
+	
 	
 	
 	private Spinner spLoadFile;
-	private ArrayAdapter<String> adpLoadFile;
+	private ArrayAdapter<File> adpLoadFile;
 	
-	private static final int SPLASH_TIME_OUT = 3000;
+	//private static final int SPLASH_TIME_OUT = 3000;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -41,39 +47,9 @@ public class SplashScreenActivity extends Activity {
 		spLoadFile = (Spinner) findViewById(R.id.sploadfile);
 		
 		//init Objects
-		adpLoadFile = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+		adpLoadFile = new ArrayAdapter<File>(this, android.R.layout.simple_list_item_1);
 		
-		ContextWrapper c = new ContextWrapper(this);
-		String strpath = c.getFilesDir().getAbsolutePath();
-		
-		String s = strpath; 
-		//pruefe, ob App-Folder existieren fuer die import/exportdateien, sonst leg ihn an
-		File ieFolder = getDir(IMPORT_EXPORT_FILE_FOLDER,MODE_PRIVATE);
-		
-		//pruefe, ob dateien vorhanden sind, ansonsten leg init-Datei an
-		if(fileList().length == 0)
-		{
-			/*
-			try {
-				
-				new InputStreamReader(getResources().openRawResource(R.raw.test));
-				
-			
-				FileOutputStream fos = openFileOutput(DEFAULT_FILE, MODE_PRIVATE);
-				fos.write(fos.);
-				
-			} catch (FileNotFoundException e) {
-				Log.e(this.getClass().getName(), e.getMessage());
-			}
-			*/
-		}
-		
-		//add all Files to Adapter and set Adapter to Spinner
-		adpLoadFile.addAll(fileList());
-		spLoadFile.setAdapter(adpLoadFile);
-		
-		adpLoadFile.setNotifyOnChange(true);
-		
+		init();
 		/*
 		 	new Handler().postDelayed(new Runnable() {
             @Override
@@ -85,6 +61,69 @@ public class SplashScreenActivity extends Activity {
         }, SPLASH_TIME_OUT);
 		 */
 	
+	}
+	
+	private void init()
+	{
+		//pruefe, ob App-Folder existieren fuer die import/exportdateien, sonst leg ihn an
+		
+		File sysFolderOnExternalStorage = new File(Environment.getExternalStorageDirectory(),
+													getResources().getString(R.string.app_name));
+		
+		if(!sysFolderOnExternalStorage.exists())
+			sysFolderOnExternalStorage.mkdir();
+		
+		File ieFolder = new File(sysFolderOnExternalStorage, IMPORT_EXPORT_FILE_FOLDER);
+		if(!ieFolder.exists())
+			ieFolder.mkdir();
+		
+		
+		if(getFileList(ieFolder,FILE_EXTENTION).length == 0)
+		{
+			int size;
+			try {
+				InputStream is = getResources().openRawResource(R.raw.test);
+				size = is.available();
+				byte[] buffer = new byte[size];
+				is.read(buffer);
+				is.close();
+				
+				
+				FileOutputStream fos = new FileOutputStream(new File(ieFolder,DEFAULT_FILE_NAME + FILE_EXTENTION));
+				fos.write(buffer);
+				fos.flush();
+				fos.close();
+				
+			} catch (IOException e) {
+				Log.e(this.getClass().getName(), e.getMessage());
+			}
+		}
+		
+		
+		//add all Files to Adapter and set Adapter to Spinner
+		adpLoadFile.addAll(getFileList(ieFolder,FILE_EXTENTION));
+		spLoadFile.setAdapter(adpLoadFile);
+		adpLoadFile.setNotifyOnChange(true);
+	}
+	
+	private File[] getFileList(File folder, String extention)
+	{
+		final String ext = extention;
+		return folder.listFiles(new FilenameFilter() {
+			
+			@Override
+			public boolean accept(File dir, String filename) {
+				
+				if(filename.toLowerCase().endsWith(ext))
+				{
+					return true;
+				}
+				else
+				{
+					return false;
+				}
+			}
+		});
 	}
 
 }
