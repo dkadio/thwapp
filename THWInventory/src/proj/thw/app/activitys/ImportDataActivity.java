a13ac61f639c8a6345e0327c581839a98b70eee9
max <max@192.168.28.108>
2014-01-19 20:31:42 +0100

    kfjg

--- THWInventory/src/proj/thw/app/activitys/ImportDataActivity.java
+++ THWInventory/src/proj/thw/app/activitys/ImportDataActivity.java
@@ -17,77 +17,61 @@ import proj.thw.app.ie.ThwCsvImporter;
 import android.os.Bundle;
 import android.os.Environment;
 import android.app.Activity;
+import android.app.TabActivity;
+import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.Spinner;
+import android.widget.TabHost;
+import android.widget.TabWidget;
 
-public class ImportDataActivity extends Activity {
+public class ImportDataActivity extends FragmentActivity {
 
 	//private static final String appFolder = "//storage//"
-	private static final String IMPORT_EXPORT_FILE_FOLDER 	= "IE";
 	private static final String DEFAULT_FILE_NAME 			= "default";
 	private static final String FILE_EXTENTION_CSV 			= ".csv";
+	private static final String FILE_EXTENTION_XML 			= ".xml";
 	
 	private enum FileType{CSV,XML};
 	private OrmDBHelper dbHelper; 
 	
+	private Spinner spTypeFile;
 	private Spinner spLoadFile;
 	private ArrayAdapter<FileIE> adpLoadFile;
+	private ArrayAdapter<FileType> adpTypeFile;
 
 	private CheckBox cbCleanDBbeforImport;
+	private TabHost tabImport;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_import_data);
 		
+	//	tabImport = (TabHost) findViewById();
+		//tabImport.setup();
+		
+		spTypeFile = (Spinner) findViewById(R.id.spformat);
+		adpTypeFile = new ArrayAdapter<FileType>(this, android.R.layout.simple_spinner_dropdown_item,FileType.values());
+		spTypeFile.setAdapter(adpTypeFile);
+		
 		spLoadFile = (Spinner) findViewById(R.id.spfilesource);
 		adpLoadFile = new ArrayAdapter<FileIE>(this, android.R.layout.simple_spinner_dropdown_item);
+		spLoadFile.setAdapter(adpLoadFile);
 		
 		cbCleanDBbeforImport = (CheckBox)findViewById(R.id.cbcleardb);
 		dbHelper = new OrmDBHelper(this);
-		init();
+		//init();
 	}
 	
+
+	/*
 	private void init()
 	{
-		//pruefe, ob App-Folder existieren fuer die import/exportdateien, sonst leg ihn an
-		File sysFolderOnExternalStorage = new File(Environment.getExternalStorageDirectory(),
-													getResources().getString(R.string.app_name));
-		
-		if(!sysFolderOnExternalStorage.exists())
-			sysFolderOnExternalStorage.mkdir();
-		
-		File ieFolder = new File(sysFolderOnExternalStorage, IMPORT_EXPORT_FILE_FOLDER);
-		if(!ieFolder.exists())
-			ieFolder.mkdir();
-		
-		
-		if(getFileList(ieFolder,FILE_EXTENTION_CSV).length == 0)
-		{
-			int size;
-			try {
-				InputStream is = getResources().openRawResource(R.raw.thwdefault);
-				size = is.available();
-				byte[] buffer = new byte[size];
-				is.read(buffer);
-				is.close();
-				
-				
-				FileOutputStream fos = new FileOutputStream(new File(ieFolder,DEFAULT_FILE_NAME + FILE_EXTENTION_CSV));
-				fos.write(buffer);
-				fos.flush();
-				fos.close();
-				
-			} catch (IOException e) {
-				Log.e(this.getClass().getName(), e.getMessage());
-			}
-		}
-		
-		
+			
 		//add all Files to Adapter and set Adapter to Spinner
 		for(File file : getFileList(ieFolder,FILE_EXTENTION_CSV))
 		{
@@ -102,26 +86,8 @@ public class ImportDataActivity extends Activity {
 		
 		spLoadFile.setAdapter(adpLoadFile);
 		adpLoadFile.setNotifyOnChange(true);
-	}
-	
-	private File[] getFileList(File folder, String extention)
-	{
-		final String ext = extention;
-		return folder.listFiles(new FilenameFilter() {
-			
-			@Override
-			public boolean accept(File dir, String filename) {
-				if(filename.toLowerCase().endsWith(ext)){
-					return true;
-				}
-				else{
-					return false;
-				}
-			}
-		});
-	}
-	
-	
+	}	
+	*/
 	public void onClickImport(View view)
 	{
 		if(cbCleanDBbeforImport.isChecked())
--- THWInventory/src/proj/thw/app/activitys/SplashScreenActivity.java
+++ THWInventory/src/proj/thw/app/activitys/SplashScreenActivity.java
@@ -1,109 +1,176 @@
 package proj.thw.app.activitys;
 
-import java.sql.SQLException;
-import java.util.ArrayList;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.io.InputStream;
 
 import proj.thw.app.R;
-import proj.thw.app.classes.Equipment;
-import proj.thw.app.database.OrmDBHelper;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
+import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class SplashScreenActivity extends Activity {
+
+	private static final String LOG 						= "SplashScreenActivity";
+	private static final int SPLASH_TIME_OUT 				= 3000;
+	private static final String IMPORT_EXPORT_FILE_FOLDER 	= "IE";
+	private static final String XML_FOLDER 					= "XML";
+	private static final String CSV_FOLDER 					= "CSV";
+	private static final String DEFAULT_FOLDER 				= "Default";
+	private static final String DEFAULT_FILE 				= "default";
+	private static final String FILE_EXTENTION_CSV 			= ".csv";
+	private static final String FILE_EXTENTION_XML 			= ".xml";
+
+	private File csvFolder;
+	private File xmlFolder;
+	private File defaultFolder;
 	
-	private static final String KEY_LOADED_LIST 	= "key.load.list";
-	private static final String KEY_EQUIPMENT_LIST 	= "key..list";
-	private static int SPLASH_TIME_OUT 				= 5000;
-	private static TextView tvStatus;
-	private static OrmDBHelper dbHelper;
 	private static Context callContext;
-	private static ProgressBar pbLoadDB;
 	
-	private static Handler postLoadDB = new Handler(){
-		public void handleMessage(android.os.Message msg) {
-			
-			Bundle bnd = msg.getData();
-			if(bnd.containsKey(KEY_LOADED_LIST))
-			{
-				ArrayList<Equipment> loadedList = (ArrayList<Equipment>)bnd.get(KEY_LOADED_LIST);
-				Intent i = new Intent(callContext,EquipmentTreeViewListActivity.class);
-				i.putExtra(KEY_EQUIPMENT_LIST, loadedList);
-				callContext.startActivity(i);
-			}
-			
-		};
-	};
-	
+	private ProgressBar pbload;
+	private TextView tvstatus;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_splash_screen);
-		
 		callContext = this;
-		/*
-		//init dbHanlder 
-		dbHelper = new OrmDBHelper(this);
 		
-		//init Views
-		tvStatus = (TextView) findViewById(R.id.tvstatus);
-		tvStatus.setVisibility(View.INVISIBLE);
+		pbload = (ProgressBar) findViewById(R.id.pbloaddb);
+		pbload.setVisibility(View.VISIBLE);
 		
-		pbLoadDB = (ProgressBar) findViewById(R.id.pbloaddb);
-		*/
-		 new Handler().postDelayed(new Runnable() {
-			 
-	            @Override
-	            public void run() {
-	                
-	            	/*
-	            	//init Handler
-	        		Thread loadDB = new Thread(new Runnable() {
-	        			
-	        			@Override
-	        			public void run() {
-	        				
-	        				ArrayList<Equipment> loadedList;
-	        				try {
-	        					tvStatus.post(new Runnable() {
-	        						
-	        						@Override
-	        						public void run() {
-	        							pbLoadDB.setVisibility(View.VISIBLE);
-	        							tvStatus.setText("Lade Datenbank...");
-	        							
-	        						}
-	        					});
-	        					loadedList = (ArrayList<Equipment>) dbHelper.getDbHelperEquip().selectAllEquipments();
-	        					Bundle bnd = new Bundle();
-	        					bnd.putSerializable(KEY_LOADED_LIST, loadedList);
-	        					Message msg = new Message();
-	        					msg.setData(bnd);
-	        					postLoadDB.handleMessage(msg);
-	        				} catch (SQLException e) {
-	        					Log.e(this.getClass().getName(), e.getMessage());
-	        					Toast.makeText(callContext, "Error beim Laden der Datenbank!", Toast.LENGTH_LONG).show();
-	        					finish();
-	        				}
-	        			}
-	        		});
-	        		
-	        		loadDB.start();
-	        		*/
-	            	
-	            	Intent i = new Intent(callContext,EquipmentTreeViewListActivity.class);
-	            	callContext.startActivity(i);
-	            }
-	        }, SPLASH_TIME_OUT);
+		tvstatus = (TextView) findViewById(R.id.tvstatus);
+		tvstatus.setText("Init Folders");
+		tvstatus.setVisibility(View.VISIBLE);
 		
-		
+		new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				writeMsg("Init Folders...");
+				Thread.currentThread();
+				try {
+					Thread.sleep(SPLASH_TIME_OUT);
+				} catch (InterruptedException e) {
+					Log.e(LOG, e.getMessage());
+				}
+				writeMsg("Init Folders...");
+				createOrCheckSysFolders();
+				writeMsg("successful!");
+				Intent i = new Intent(callContext,
+						EquipmentTreeViewListActivity.class);
+				callContext.startActivity(i);
+				
+			}
+		}).start();
 	}
+	
+	private void writeMsg(final String msg)
+	{
+		tvstatus.post(new Runnable() {
+			@Override
+			public void run() {
+				tvstatus.setText(msg);
+			}
+		});
+	}
+
+	private void createOrCheckSysFolders() {
+		// pruefe, ob App-Folder existieren fuer die import/exportdateien, sonst
+		// leg ihn an
+		File sysFolderOnExternalStorage = new File(
+				Environment.getExternalStorageDirectory(), getResources()
+						.getString(R.string.app_name));
+
+		if (!sysFolderOnExternalStorage.exists())
+			sysFolderOnExternalStorage.mkdir();
+
+		File ieFolder = new File(sysFolderOnExternalStorage,
+				IMPORT_EXPORT_FILE_FOLDER);
+		if (!ieFolder.exists())
+			ieFolder.mkdir();
+
+		csvFolder = new File(ieFolder, CSV_FOLDER);
+		if (!csvFolder.exists())
+			csvFolder.mkdir();
+		
+		createDefaultCSV();
+		
+		xmlFolder = new File(ieFolder, XML_FOLDER);
+		if (!xmlFolder.exists())
+			xmlFolder.mkdir();
+
+		createDefaultXML();
+	}
+
+	private void createDefaultCSV() {
+
+		defaultFolder = new File(csvFolder, DEFAULT_FOLDER);
+		if (!defaultFolder.exists()) {
+			defaultFolder.mkdir();
+		}
+
+		createDefaultFiles(defaultFolder, DEFAULT_FILE, FILE_EXTENTION_CSV,
+				R.raw.thwdefault);
+	}
+
+	private void createDefaultXML() {
+
+		defaultFolder = new File(xmlFolder, DEFAULT_FOLDER);
+		if (!defaultFolder.exists()) {
+			defaultFolder.mkdir();
+		}
+
+		createDefaultFiles(defaultFolder, DEFAULT_FILE, FILE_EXTENTION_XML, -1);
+	}
+
+	private void createDefaultFiles(File parentFolder, String fileName,
+			String fileExtention, int resId) {
+
+		if (resId >= 0) {
+			if (getFileList(parentFolder, fileExtention).length == 0) {
+				int size;
+				try {
+					InputStream is = getResources().openRawResource(resId);
+					size = is.available();
+					byte[] buffer = new byte[size];
+					is.read(buffer);
+					is.close();
+
+					FileOutputStream fos = new FileOutputStream(new File(
+							parentFolder, fileName + fileExtention));
+					fos.write(buffer);
+					fos.flush();
+					fos.close();
+
+				} catch (IOException e) {
+					Log.e(this.getClass().getName(), e.getMessage());
+				}
+			}
+		}
+	}
+
+	private File[] getFileList(File folder, String extention) {
+		final String ext = extention;
+		return folder.listFiles(new FilenameFilter() {
+
+			@Override
+			public boolean accept(File dir, String filename) {
+				if (filename.toLowerCase().endsWith(ext)) {
+					return true;
+				} else {
+					return false;
+				}
+			}
+		});
+	}
+
 }
