c9ba09478316130ba4f3f86518bbe2b9e87f77c0
max <max@192.168.28.105>
2014-01-23 01:17:53 +0100

    rekursives laden der items ab selectierter Ebene Tree Adapter angepasst, damit uber go butten nur selektierte eintraege geladen werden sonstige anpassungen....

--- THWInventory/src/proj/thw/app/adapters/ThwTreeViewAdapter.java
+++ THWInventory/src/proj/thw/app/adapters/ThwTreeViewAdapter.java
@@ -1,5 +1,6 @@
 package proj.thw.app.adapters;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import proj.thw.app.R;
@@ -18,95 +19,102 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class ThwTreeViewAdapter extends AbstractTreeViewAdapter<Equipment>{
-	
-	 private final Set<Equipment> selected;
+public class ThwTreeViewAdapter extends AbstractTreeViewAdapter<Equipment> {
 
-	    private final OnCheckedChangeListener onCheckedChange = new OnCheckedChangeListener() {
-	        @Override
-	        public void onCheckedChanged(final CompoundButton buttonView,
-	                final boolean isChecked) {
-	            final Equipment id = (Equipment) buttonView.getTag();
-	            changeSelected(isChecked, id);
-	        }
+	private Set<Equipment> selected;
 
-	    };
-
-	    private void changeSelected(final boolean isChecked, final Equipment id) {
-	        if (isChecked) {
-	            selected.add(id);
-	        } else {
-	            selected.remove(id);
-	        }
-	    }
-
-	    public ThwTreeViewAdapter(final EquipmentTreeViewListActivity treeViewList,
-	            final Set<Equipment> selected,
-	            final TreeStateManager<Equipment> treeStateManager,
-	            final int numberOfLevels) {
-	        super(treeViewList, treeStateManager, numberOfLevels);
-	        this.selected = selected;
-	    }
-
-	    private String getDescription(final Equipment id) {
-	        final Integer[] hierarchy = getManager().getHierarchyDescription(id);
-	        return id.toString(); //+ Arrays.asList(hierarchy);
-	    }
-
-	    @Override
-	    public View getNewChildView(final TreeNodeInfo<Equipment> treeNodeInfo) {
-	        final LinearLayout viewLayout = (LinearLayout) getActivity()
-	                .getLayoutInflater().inflate(R.layout.tree_list_item, null);
-	        return updateView(viewLayout, treeNodeInfo);
-	    }
-
-	    @Override
-	    public LinearLayout updateView(final View view,
-	            final TreeNodeInfo<Equipment> treeNodeInfo) {
-	        final LinearLayout viewLayout = (LinearLayout) view;
-	        final TextView descriptionView = (TextView) viewLayout
-	                .findViewById(R.id.tree_list_item_description);
-	        final TextView levelView = (TextView) viewLayout
-	                .findViewById(R.id.tree_list_item_level);
-	        final ImageView itemImage = (ImageView) viewLayout
-	                .findViewById(R.id.treeitemimg);
-	        descriptionView.setText(getDescription(treeNodeInfo.getId()));
-	        levelView.setText(Integer.toString(treeNodeInfo.getLevel()));
-	        if(treeNodeInfo.getId().getEquipImg().getImg() != null)
-	        	itemImage.setImageBitmap(treeNodeInfo.getId().getEquipImg().getImg());
-	        final CheckBox box = (CheckBox) viewLayout
-	                .findViewById(R.id.tree_list_checkbox);
-	        box.setTag(treeNodeInfo.getId());
-	        if (treeNodeInfo.isWithChildren()) {
-	            box.setVisibility(View.GONE);
-	        } else {
-	            box.setVisibility(View.VISIBLE);
-	            box.setChecked(selected.contains(treeNodeInfo.getId()));
-	        }
-	        box.setOnCheckedChangeListener(onCheckedChange);
-	        return viewLayout;
-	    }
-
-	    @Override
-	    public void handleItemClick(final View view, final Object id) {
-	        final Equipment longId = (Equipment) id;
-	        final TreeNodeInfo<Equipment> info = getManager().getNodeInfo(longId);
-	        if (info.isWithChildren()) {
-	            super.handleItemClick(view, id);
-	        } else {
-	            final ViewGroup vg = (ViewGroup) view;
-	            final CheckBox cb = (CheckBox) vg
-	                    .findViewById(R.id.tree_list_checkbox);
-	            cb.performClick();
-	        }
-	    }
-
+	private final OnCheckedChangeListener onCheckedChange = new OnCheckedChangeListener() {
 		@Override
-		public long getItemId(int position) {
-			// TODO Auto-generated method stub
-			return 0;
+		public void onCheckedChanged(final CompoundButton buttonView,
+				final boolean isChecked) {
+			final Equipment id = (Equipment) buttonView.getTag();
+			changeSelected(isChecked, id);
 		}
 
+	};
 
+	private void changeSelected(final boolean isChecked, final Equipment id) {
+		if (isChecked) {
+			selected.add(id);
+		} else {
+			selected.remove(id);
+		}
+	}
 
+	public ThwTreeViewAdapter(final EquipmentTreeViewListActivity treeViewList,
+			Set<Equipment> selected,
+			final TreeStateManager<Equipment> treeStateManager,
+			final int numberOfLevels) {
+		super(treeViewList, treeStateManager, numberOfLevels);
+		if (selected == null) {
+			this.selected = new HashSet<Equipment>();
+		} else {
+			this.selected = selected;
+		}
+
+	}
+
+	private String getDescription(final Equipment id) {
+		final Integer[] hierarchy = getManager().getHierarchyDescription(id);
+		return id.toString(); // + Arrays.asList(hierarchy);
+	}
+
+	@Override
+	public View getNewChildView(final TreeNodeInfo<Equipment> treeNodeInfo) {
+		final LinearLayout viewLayout = (LinearLayout) getActivity()
+				.getLayoutInflater().inflate(R.layout.tree_list_item, null);
+		return updateView(viewLayout, treeNodeInfo);
+	}
+
+	@Override
+	public LinearLayout updateView(final View view,
+			final TreeNodeInfo<Equipment> treeNodeInfo) {
+		final LinearLayout viewLayout = (LinearLayout) view;
+		final TextView descriptionView = (TextView) viewLayout
+				.findViewById(R.id.tree_list_item_description);
+		final TextView levelView = (TextView) viewLayout
+				.findViewById(R.id.tree_list_item_level);
+		final ImageView itemImage = (ImageView) viewLayout
+				.findViewById(R.id.treeitemimg);
+		descriptionView.setText(getDescription(treeNodeInfo.getId()));
+		levelView.setText(Integer.toString(treeNodeInfo.getLevel()));
+		if (treeNodeInfo.getId().getEquipImg().getImg() != null)
+			itemImage.setImageBitmap(treeNodeInfo.getId().getEquipImg()
+					.getImg());
+		final CheckBox box = (CheckBox) viewLayout
+				.findViewById(R.id.tree_list_checkbox);
+		box.setTag(treeNodeInfo.getId());
+		if (treeNodeInfo.isWithChildren()) {
+			box.setVisibility(View.GONE);
+		} else {
+			box.setVisibility(View.VISIBLE);
+			box.setChecked(selected.contains(treeNodeInfo.getId()));
+		}
+		box.setOnCheckedChangeListener(onCheckedChange);
+		return viewLayout;
+	}
+
+	@Override
+	public void handleItemClick(final View view, final Object id) {
+		final Equipment longId = (Equipment) id;
+		final TreeNodeInfo<Equipment> info = getManager().getNodeInfo(longId);
+		if (info.isWithChildren()) {
+			super.handleItemClick(view, id);
+		} else {
+			final ViewGroup vg = (ViewGroup) view;
+			final CheckBox cb = (CheckBox) vg
+					.findViewById(R.id.tree_list_checkbox);
+			//cb.performClick();
+		}
+	}
+
+	@Override
+	public long getItemId(int position) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	public Set<Equipment> getSelected() {
+		return selected;
+		}
 }
